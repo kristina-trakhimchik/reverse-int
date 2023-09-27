c9c83eed1b1081497cefd41452f3d14da29d836b
kristina-trakhimchik <dilfinium_kristi@mail.ru>
2023-09-28 00:39:39 +0300

    implement reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  let ar = n.toString();
+
+  let result = [];
+  for (let i = ar.length - 1; i >= 0; i--) {
+    result.push(ar[i]);
+  }
+  return parseInt(result.join(''));
 }
